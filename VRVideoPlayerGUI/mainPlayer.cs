070d250bac4b6ced4c2e11af69eee563b1a6fc17
gusman <geniwab@gmail.com>
2016-11-08 01:17:51 +0100

    Removed old code for VRPlayer

--- VRVideoPlayerGUI/mainPlayer.cs
+++ VRVideoPlayerGUI/mainPlayer.cs
@@ -22,56 +22,9 @@ namespace VRVideoPlayerGUI
         public mainPlayer()
         {
             InitializeComponent();
-            Listen();
-
 
         }
-
-        UdpClient client;
-        unsafe void Listen()
-        {
-            bool ip = false;
-            bool pt = false;
-
-            IPAddress address = IPAddress.Any;
-            int port = 9090;
-
-            IPEndPoint ep = new IPEndPoint(address, port);
-
-            client = new UdpClient();
-            client.EnableBroadcast = true;
-            client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-            client.ExclusiveAddressUse = false;
-            client.Client.Bind(ep);
-
-            Task.Run(() =>
-            {
-                try
-                {
-                    while (true)
-                    {
-
-
-                        if (player != null)
-                        {
-                            byte[] data = client.Receive(ref ep);
-                            string sData = Encoding.UTF8.GetString(data);
-                            PSVRSensor sensor = JsonConvert.DeserializeObject<PSVRSensor>(sData);
-
-                            if (sensor.Orientation == null)
-                                continue;
-
-                            fixed (float* d = &sensor.Orientation[0])
-                                player.RotateMesh(d);
-                        }
-                        else
-                            Thread.Sleep(10);
-                    }
-                }
-                catch { }
-            });
-
-        }
+        
         private void button1_Click(object sender, EventArgs e)
         {
 
