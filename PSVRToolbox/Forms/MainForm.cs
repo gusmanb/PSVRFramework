ac9b54ad5c68ca8507987e104a46be0f41ed02a9
gusman <geniwab@gmail.com>
2016-11-10 22:26:34 +0100

    Removed unused reference

--- PSVRToolbox/Forms/MainForm.cs
+++ PSVRToolbox/Forms/MainForm.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Numerics;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
